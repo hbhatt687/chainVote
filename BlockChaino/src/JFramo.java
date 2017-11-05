20322d969962c21e6659f2cd06cd24dfd69f10a0
ahsia3 <tonyhsia22@gmail.com>
2017-11-04 23:34:32 -0400

    made finishing touches towards Program of JFramo and chainCode

--- BlockChaino/src/JFramo.java
+++ BlockChaino/src/JFramo.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -10,30 +12,32 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class JFramo extends KeyAdapter{
-	
+
 	static JFrame frame;
-	static JButton button1;
+	static JButton b1;
 	static JPanel panel;
-	static boolean i=false;
+	JFrame window=chainCode.frame1;
 	
-	
-	//If space is pressed then i=true and go into if statement to execute the next JFrame in main
+	//If space is pressed then close this JFRAME to execute the next JFrame in main
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-               i=true;
-         }
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_SPACE) {
+			new chainCode(frame);
+			frame.setVisible(false);
+			window.setVisible(true);
+			frame.dispose();
+			
+		}
 	}
-	
-	public static void main(String[]args) {
+	public JFramo() {
 		//UI for Setting up Box
-		frame = new JFrame("Not a tutorial");
-		System.out.println("Lets see if this does not work");
+		frame = new JFrame("ChainVotes");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		frame.setPreferredSize(new Dimension(1000, 500));
+		frame.getContentPane().add(new StyleOptionsPanel());
 
 		frame.pack();
 		frame.setVisible(true);
@@ -42,44 +46,76 @@ public class JFramo extends KeyAdapter{
 		frame.getContentPane().setBackground(Color.BLACK);
 		//----End of JFrame
 		
-		//adding panel and button for Frame
-		panel = new JPanel();
-	//	button1 = new JButton("Click me because I'm not a tutorial");
-		panel.setLayout(null);
-		
-		//First Label
-		JLabel label = new JLabel("Logged In");
-		panel.add(label);
-		label.setBounds(430, 20, 900, 900);
-		label.setFont(new Font("Serif", Font.BOLD, 80));
-		label.setForeground(Color.cyan);
-
-		//2nd Label 
-		JLabel label2 = new JLabel("Press SPACE key to continue");
-		panel.add(label2);
-		label2.setBounds(430, 90, 900, 900);
-		label2.setFont(new Font("Serif", Font.BOLD, 20));
-		label2.setForeground(Color.cyan);
-		
-		frame.add(panel);
-		panel.setPreferredSize(new Dimension(1200,980));
-		panel.setBorder(BorderFactory.createLineBorder(Color.CYAN));
-	//	button1.setBounds(250,400,120,120);
-	//	panel.add(button1);
-		panel.setBackground(Color.DARK_GRAY);
-		
-		
-		//Trying to close this JFrame and run other class (chainCode)
-		if (i==true) {
-			 SwingUtilities.invokeLater(new Runnable() {
-		            public void run() {
-		            	frame.setVisible(false);
-		                new chainCode();
-		            }
-		        });
-		}
-		
-	}
-
 	
+	}
+	
+	public static void main(String[]args) {
+		new JFramo();
+	}
+	
+	//Trying to close this JFrame and run other class (chainCode)
+	/*if (i==true) {
+		 SwingUtilities.invokeLater(new Runnable() {
+			 @Override
+	            public void run() {
+	            	//frame.setVisible(false);
+	                chainCode please = new chainCode();
+	                please.newScreen();
+	            }
+	        });
+	}
+	*/
 }
+
+class StyleOptionsPanel extends JPanel{
+	JFramo framo;
+	static JButton b1;
+	static JPanel panel;
+	JFrame window=chainCode.frame1;
+
+public StyleOptionsPanel(){
+	b1 = new JButton("CONTINUE");
+	b1.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e){
+			
+			if(e.getSource()==b1){
+				new chainCode(framo.frame);
+				framo.frame.setVisible(false);
+			}
+		}
+	});
+		
+
+			
+			//adding panel and button for Frame
+			
+			setLayout(null);
+			
+			//First Label
+			JLabel label = new JLabel("Logged In");
+			add(label);
+			label.setBounds(430, 20, 900, 900);
+			label.setFont(new Font("Serif", Font.BOLD, 80));
+			label.setForeground(Color.cyan);
+
+			//2nd Label 
+			/*JLabel label2 = new JLabel("Press SPACE key to continue");
+			add(label2);
+			label2.setBounds(430, 90, 900, 900);
+			label2.setFont(new Font("Serif", Font.BOLD, 20));
+			label2.setForeground(Color.cyan);*/
+			
+			//panel customization
+			setPreferredSize(new Dimension(1200,980));
+			setBorder(BorderFactory.createLineBorder(Color.CYAN));
+			setBackground(Color.DARK_GRAY);
+			
+			add(b1);
+			b1.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+			b1.setForeground(Color.cyan);
+			b1.setBounds(515,550,150,50);
+			b1.setBackground(Color.BLACK);
+			b1.setFont(new Font("Serif", Font.BOLD, 25));
+}
+}
+
--- BlockChaino/src/chainCode.java
+++ BlockChaino/src/chainCode.java
@@ -1,26 +1,105 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class chainCode {
-	static JFrame frame;
-	public static void main(String[] args) {
-		System.out.println("Key, Value");
+public class chainCode{
+	static JFrame frame1;
+	static JPanel panel1 = new JPanel();
+	static JPanel panel2 = new JPanel();
+	static JPanel panel3 = new JPanel();
+	static JButton button1, button2;
+	static JLabel lb, lb1, lb2, lb3;
+	
+	public chainCode(JFrame frame){
+		this.frame1 = frame;
 		//UI for Setting up Box
-				frame = new JFrame("Not a tutorial");
-				System.out.println("Lets see if this does not work");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-				frame.setPreferredSize(new Dimension(1000, 500));
-
-				frame.pack();
-				frame.setVisible(true);
-				frame.setLayout(null);
-				frame.setLayout(new FlowLayout());
-				frame.getContentPane().setBackground(Color.BLACK);
-				//----End of JFrame
+		frame1 = new JFrame("ChainVoting");
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame1.setPreferredSize(new Dimension(1000, 500));
+		frame1.pack();
+		frame1.setVisible(true);
+		frame1.setLayout(null);
+		frame1.setLayout(new FlowLayout());
+		frame1.getContentPane().setBackground(Color.BLACK);
+		//----End of JFrame
+		frame1.pack();
+		frame1.setLocationRelativeTo(null);
+		frame1.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+				
+		frame1.add(panel1);		
+		frame1.add(panel2);
+		frame1.add(panel3);
+		
+		lb = new JLabel("Select Preferred Bill");
+		lb.setFont(new Font("Serif", Font.BOLD, 80));
+		lb.setForeground(Color.cyan);
+		
+		lb1 = new JLabel("Description of Bill A");
+		lb1.setFont(new Font("Serif", Font.BOLD, 35));
+		lb1.setForeground(Color.white);
+		
+		lb2 = new JLabel("Description of Bill B");
+		lb2.setFont(new Font("Serif", Font.BOLD, 35));
+		lb2.setForeground(Color.white);
+		
+		panel3.setPreferredSize(new Dimension(1200,220));
+		panel3.setBorder(BorderFactory.createLineBorder(Color.white));
+		panel3.setBackground(Color.CYAN);
+		panel3.setLayout(new BorderLayout());
+		
+		panel2.setPreferredSize(new Dimension(1200,220));
+		panel2.setBorder(BorderFactory.createLineBorder(Color.white));
+		panel2.setBackground(Color.CYAN);
+		panel2.setLayout(new BorderLayout());
+		
+		panel1.setPreferredSize(new Dimension(1000,280));
+		panel1.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		panel1.setBackground(Color.DARK_GRAY);
+		panel1.setLayout(new GridBagLayout());
+		panel1.add(lb);
+		
+		button1 = new JButton("Bill A");
+		button1.setBounds(250,300,300,90);
+		button1.setFont(new Font("Serif", Font.BOLD, 35));
+		panel2.add(button1, BorderLayout.LINE_START);
+		panel2.add(lb1);
+		
+		
+		button2 = new JButton("Bill B");
+		button2.setBounds(10,500,200,90);
+		button2.setFont(new Font("Serif", Font.BOLD, 35));
+		panel3.add(button2, BorderLayout.LINE_START);
+		panel3.add(lb2);
+	
+	}
+	
+	public static void pickA() {
+		button1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+					System.out.println("Voter has chosen Bill A");
+			}
+		});
+		
+		button2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+					System.out.println("Voter has chosen Bill B");
+			}
+		});
+	}
+	public static void main(String[]args) {
+		new chainCode(frame1);
+		pickA();
 	}
 
 }
